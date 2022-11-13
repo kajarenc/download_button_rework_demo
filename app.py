da20f27856880dc705d807c378314f69f92fd956
Karen Javadyan <kajarenc@gmail.com>
2022-11-13 19:05:09 +0400

    add title

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.title("CHECK STATIC FILES")
+
 text_to_repeat = st.text_input("Text to repeat")
 
 times_to_repeat = st.slider("Number of times to repeat", min_value=1, max_value=10)
